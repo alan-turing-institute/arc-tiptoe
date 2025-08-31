d9c1c5ab09f2cf42f50c8b7891ff43384db44cb0
eddableheath <edable-heath@turing.ac.uk>
2025-08-31 22:23:00 +0100

    :recycle::bug::rocket: minor refactor, bug fix, and feature addition to full pipeline

--- src/arc_tiptoe/preprocessing/pipeline/full_pipeline.py
+++ src/arc_tiptoe/preprocessing/pipeline/full_pipeline.py
@@ -27,54 +27,58 @@ class PreprocessingPipeline:
         self.logger = logging.getLogger(__name__)
         self.logger.info("Initializing preprocessing pipeline")
 
-        self.embedder = embedders.embedders[self.config.embed_lib](
-            self.config, within_pipeline=True
-        )
-
         # load these when appropriate
+        self.embedder = None
         self.dim_reducer = None
         self.clusterer = None
 
-    def run(self):
-        """Run full preprocessing pipeline."""
+    def _embed(self):
+        """Run embedding step."""
         if self.config.embedding_done:
             self.logger.info("Skipping embedding step, already done")
-        else:
-            self.logger.info("Starting embedding step%s", "==" * 20)
-            self.embedder.load_dataset()
-            self.config = self.embedder.embed()
+            return
+
+        self.embedder = embedders.embedders[self.config.embed_lib]
+        self.logger.info("Starting embedding step%s", "==" * 20)
+        self.embedder.load_dataset()
+        self.config = self.embedder.embed()
+
+    def _cluster(self):
+        """Run clustering step."""
+        if self.config.clustering_done:
+            self.logger.info("Skipping clustering step, already done")
+            return
+
+        self.clusterer = clusterers.clusterers[self.config.cluster["cluster_method"]](
+            self.config
+        )
+        self.logger.info("Starting clustering step%s", "==" * 20)
+        self.config = self.clusterer.cluster_and_assign()
+
+    def _dim_reduce(self):
+        """Run dimensionality reduction step."""
+        if self.config.dim_red["dim_red_done"]:
+            self.logger.info("Skipping dimensionality reduction step, already done")
+            return
+
+        self.dim_reducer = dim_reducers.dim_reducers[
+            self.config.dim_red["dim_red_method"]
+        ](self.config)
+        self.logger.info("Starting dimensionality reduction step%s", "==" * 20)
+        self.config = self.dim_reducer.reduce_dimensions()
+
+    def run(self):
+        """Run full preprocessing pipeline."""
+        self._embed()
 
         if self.config.dim_red["dim_red_before_clustering"]:
             self.logger.info("Running dimensionality reduction before clustering")
-            self.logger.info("Updating config after embedding step")
-            self.dim_reducer = dim_reducers.dim_reducers[
-                self.config.dim_red["dim_red_method"]
-            ](self.config, within_pipeline=True)
-
-            self.logger.info("Starting dimensionality reduction step%s", "==" * 20)
-            self.config = self.dim_reducer.reduce_dimensions()
-
-            self.logger.info("Starting clustering step%s", "==" * 20)
-            self.logger.info("Updating config after dim reduction step")
-            self.clusterer = clusterers.clusterers[self.config.cluster_method](
-                self.config
-            )
-            self.clusterer.cluster_and_assign()
+            self._dim_reduce()
+            self._cluster()
         else:
             self.logger.info("Running clustering before dimensionality reduction")
-            self.logger.info("Updating config after embedding step")
-            self.clusterer = clusterers.clusterers[self.config.cluster_method](
-                self.config, within_pipeline=True
-            )
-            self.logger.info("Starting clustering step%s", "==" * 20)
-            self.config = self.clusterer.cluster_and_assign()
-
-            self.logger.info("Updating config after clustering step")
-            self.dim_reducer = dim_reducers.dim_reducers[
-                self.config.dim_red["dim_red_method"]
-            ](self.config, within_pipeline=True)
-            self.logger.info("Starting dimensionality reduction step%s", "==" * 20)
-            self.dim_reducer.reduce_dimensions()
+            self._cluster()
+            self._dim_reduce()
 
         self.logger.info("%s", "==" * 20)
         self.logger.info("Preprocessing pipeline complete")
