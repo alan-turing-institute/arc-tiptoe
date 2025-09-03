00d05dfc230f8806f47edce8a4d21a961955b21b
eddableheath <edable-heath@turing.ac.uk>
2025-09-03 16:06:34 +0100

    :construction: draft dim_reduce_clusters fix added to original tiptoe

--- original_tiptoe/dim-reduce/dim_reduce_url_clusters.py
+++ original_tiptoe/dim-reduce/dim_reduce_url_clusters.py
@@ -115,3 +115,37 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+PCA_COMPONENTS_FILE = "/work/edauterman/pca_%d.npy" % (NEW_DIM)
+DIRECTORY = "/work/edauterman/test_boundary_url_bundles_2_dotprod/"
+OUT_DIRECTORY = "/work/edauterman/test_boundary_url_bundles_2_dotprod_pca_192/"
+
+pca_components = numpy.load(PCA_COMPONENTS_FILE)
+
+directories = glob.glob("%s/*" % DIRECTORY)
+files = []
+if not os.path.exists(OUT_DIRECTORY):
+    os.mkdir(OUT_DIRECTORY)
+print(directories)
+for directory in directories:
+    print(directory)
+    files += glob.glob("%s/clusters/*" % directory)
+    if not os.path.exists(re.sub(r"%s" % DIRECTORY, r"%s" % OUT_DIRECTORY, directory)):
+        os.mkdir(re.sub(r"%s" % DIRECTORY, r"%s" % OUT_DIRECTORY, directory))
+
+    if not os.path.exists(
+        re.sub(r"%s" % DIRECTORY, r"%s" % OUT_DIRECTORY, "%s/clusters" % directory)
+    ):
+        os.mkdir(
+            re.sub(r"%s" % DIRECTORY, r"%s" % OUT_DIRECTORY, "%s/clusters" % directory)
+        )
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
+    for i, f in enumerate(files):
+        executor.submit(
+            transform_embeddings,
+            pca_components,
+            f,
+            re.sub(r"%s" % DIRECTORY, r"%s" % OUT_DIRECTORY, f),
+        )
