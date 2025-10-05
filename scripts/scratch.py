ce1c68d00d7d28b6df3ada587d5f32d31035e896
joannacknight <jknight@turing.ac.uk>
2025-10-05 18:35:09 +0100

    removed scratch file

--- scripts/scratch.py
+++
@@ -1,8 +0,0 @@
-from arc_tiptoe.preprocessing.clustering.clustering import KMeansClusterer
-from arc_tiptoe.preprocessing.utils.config import PreProcessConfig
-
-json_config_path = "configs/distilbert_post_embedding.json"
-config = PreProcessConfig(json_config_path)
-
-clusterer = KMeansClusterer(config, within_pipeline=True)
-print('here')
