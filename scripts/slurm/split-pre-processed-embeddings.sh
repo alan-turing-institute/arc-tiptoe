2ef45c2e2e9743660b80acf5d589aaa055cca33c
J-Dymond <jackdymond@me.com>
2025-10-10 15:48:39 +0100

    added slurm script

---
+++ scripts/slurm/split-pre-processed-embeddings.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --account vjgo8416-co-beagle
+#SBATCH --qos turing
+#SBATCH --job-name preprocess-queries
+#SBATCH --time 0-1:0:0
+#SBATCH --nodes 1
+#SBATCH --gpus 1
+#SBATCH --cpus-per-gpu 36
+#SBATCH --output /bask/projects/v/vjgo8416-co-beagle/slurm_logs/split_queries-%j.out
+
+# Load required modules here
+module purge
+module load baskerville
+
+# Activate venv
+source /bask/projects/v/vjgo8416-co-beagle/arc-tiptoe/.venv/bin/activate
+
+export IR_DATASETS_TMP=/bask/projects/v/vjgo8416-co-beagle/tmp_ir_datasets
+export IR_DATASETS_HOME=/bask/projects/v/vjgo8416-co-beagle/ir_datasets_cache
+export HUGGINGFACE_HUB_CACHE=/bask/projects/v/vjgo8416-co-beagle/.cache
+
+python3 arc-exps/split_query_csvs.py $1
