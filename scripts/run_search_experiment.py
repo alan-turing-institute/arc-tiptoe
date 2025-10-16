8b9f17a76c161bd786e8d29042e67f6e401d620a
eddableheath <edable-heath@turing.ac.uk>
2025-10-16 11:57:23 +0100

    :rocket: removed async code

--- scripts/run_search_experiment.py
+++ scripts/run_search_experiment.py
@@ -2,39 +2,13 @@
 Run a search experiment with specified parameters.
 """
 
-import asyncio
-
 import jsonargparse
 
-from arc_tiptoe.search.search_experiment import (
-    SearchExperimentAsync,
-    SearchExperimentSingleThread,
-)
+from arc_tiptoe.search.search_experiment import SearchExperimentSingleThread
 from arc_tiptoe.search.servers import TiptoeServerManager, aggressive_cleanup
 
 
-async def main(
-    config_path: str,
-    search_dir: str = "search",
-    dataset_name: str = "msmarco-passage/dev/small",
-):
-    """Main entry point for the script."""
-
-    # Clean up previous processes and start servers
-    aggressive_cleanup()
-    server_manager = TiptoeServerManager(config_path, search_dir)
-    if not server_manager.start_servers():
-        print("Error: Failed to start servers.")
-        return 1
-
-    # Run the search experiment
-    experiment = SearchExperimentAsync(config_path, search_dir, dataset_name)
-    await experiment.run_experiment(output_path="search_results.csv")
-
-    return 1
-
-
-def main_sync(
+def main(
     config_path: str,
     search_dir: str = "search",
     queries_path: str = "scratch/msmarco-queries-embedded.csv",
@@ -67,13 +41,9 @@ if __name__ == "__main__":
     arg_parser.add_argument("--save_path", type=str, default="search_results.csv")
     arg_parser.add_argument("--search_dir", type=str, default="search")
     args = arg_parser.parse_args()
-    async_mode = False
-    if async_mode:
-        asyncio.run(main(config_path=args.json_search_config_path))
-    else:
-        main_sync(
-            config_path=args.json_search_config_path,
-            search_dir=args.search_dir,
-            queries_path=args.queries_path,
-            save_path=args.save_path,
-        )
+    main(
+        config_path=args.json_search_config_path,
+        search_dir=args.search_dir,
+        queries_path=args.queries_path,
+        save_path=args.save_path,
+    )
--- src/arc_tiptoe/search/search_experiment.py
+++ src/arc_tiptoe/search/search_experiment.py
@@ -2,7 +2,6 @@
 Full search experiment class.
 """
 
-import asyncio
 import json
 import os
 import subprocess
@@ -10,105 +9,12 @@ from pathlib import Path
 from uuid import uuid4
 
 import faiss
-import ir_datasets
 import numpy as np
 import pandas as pd
 import torch
-from tqdm import tqdm as tqdm_sync
-from tqdm.asyncio import tqdm
+from tqdm import tqdm
 
 from arc_tiptoe.preprocessing.embedding import tt_models
-
-
-class SearchExperimentAsync:
-    """Class to run a full search experiment"""
-
-    def __init__(
-        self,
-        config_path: str,
-        search_dir: str = "search",
-        dataset_name: str = "msmarco-passage/dev/small",
-    ):
-        self.config_path = config_path
-        with open(config_path, encoding="utf-8") as f:
-            self.config = json.load(f)
-
-        self.search_dir = Path(search_dir).resolve()
-        self.dataset_name = dataset_name
-        self.queries = []
-        self.cluster_search_num = self.config["clustering"].get("search_top_k", 4)
-        self.results_df = pd.DataFrame(
-            columns=["query_id"]
-            + [f"cluster_{i+1}_res" for i in range(self.cluster_search_num)]
-            + [f"cluster_{i+1}_total_comm" for i in range(self.cluster_search_num)]
-        )
-        self.results = []
-        self.num_clusters = self.config["clustering"].get("total_clusters")
-
-        self._load_queries()
-
-    def _load_queries(self):
-        query_iter = ir_datasets.load(self.dataset_name).queries_iter()
-        self.queries = list(query_iter)
-
-    async def _single_query_search(self, query):
-        """Run search for a single query."""
-        cmd = [
-            "go",
-            "run",
-            "main.go",
-            "--search_config",
-            f"../{self.config_path}",
-            "multi-cluster-experiment",
-            query[1],
-        ]
-
-        process = await asyncio.create_subprocess_exec(
-            *cmd,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-            cwd=self.search_dir,
-        )
-
-        stdout, stderr = await process.communicate()
-
-        print(stderr.decode())
-
-        output = stdout.decode().splitlines()
-        print(output)
-        result = self._parse_go_output(output[-1], query[0])
-        self.results.append(result)
-        return result
-
-    def _parse_go_output(self, output, query_id):
-        """Parse the output from the Go program and return a structured dictionary."""
-
-        results = json.loads(output.splitlines()[-1])
-        structured_results = {"query_id": query_id}
-        for cluster_res in results["all_results"]:
-            structured_results[f"cluster_{cluster_res['cluster_rank']}_res"] = (
-                cluster_res["results"]
-            )
-            structured_results[f"cluster_{cluster_res['cluster_rank']}_total_comm"] = (
-                cluster_res["perf_up"] + cluster_res["perf_down"]
-            )
-        return structured_results
-
-    async def run_experiment(self, output_path: str | None = None):
-        """Run the full search experiment."""
-        tasks = [self._single_query_search(query) for query in self.queries]
-        await tqdm.gather(*tasks)
-        for result in self.results:
-            self.results_df = pd.concat(
-                [self.results_df, pd.DataFrame([result])], ignore_index=True
-            )
-        if output_path is not None:
-            self._save_results(output_path)
-        return self.results_df
-
-    def _save_results(self, output_path: str):
-        """Save the results DataFrame to a CSV file."""
-        self.results_df.to_csv(output_path, index=False)
 
 
 class SearchExperimentSingleThread:
@@ -168,6 +74,7 @@ class SearchExperimentSingleThread:
             )
         else:
             pca_components_path = self.config["dim_reduction"]["pca_components_file"]
+            print(f"Loading PCA components from {pca_components_path}")
         return np.load(pca_components_path)
 
     def _load_cluster_index(self):
@@ -301,7 +208,7 @@ class SearchExperimentSingleThread:
         """Run the full search experiment."""
         _ = [
             self._single_query_search(query)
-            for _, query in tqdm_sync(self.queries.iterrows())
+            for _, query in tqdm(self.queries.iterrows())
         ]
         for result in self.results:
             self.results_df = pd.concat(
@@ -313,4 +220,5 @@ class SearchExperimentSingleThread:
 
     def _save_results(self, output_path: str):
         """Save the results DataFrame to a CSV file."""
+        self.results_df.to_csv(output_path, index=False)
         self.results_df.to_csv(output_path, index=False)
